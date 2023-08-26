80d145496fbcbb55f58c94e0f52757812e228c03
qinisonhlakaniphoxulu <qiniso.nhlakanipho.xulu@gmail.com>
2023-08-26 23:39:19 +0200

    My first commit

---
+++ src/client.js
@@ -0,0 +1,6 @@
+import sanityClient from '@sanity/client'
+
+export default sanityClient({
+    projectId: "j0uyy8d7",
+    dataset: "production"
+})
---
+++ studio/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'j0uyy8d7',
+    dataset: 'production'
+  }
+})
